eb2af5db77e60b0ccf0d469176d448316249cf71
Nazim Uddin <nushourav4@gmail.com>
2021-11-21 15:00:40 +0900

    registration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Component/Pages/Home/Home/Home";
+import Login from "./Component/Pages/Login/Login";
+import Registration from "./Component/Pages/Registration/Registration";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/registration" element={<Registration />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Component/Pages/Registration/Registration.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Registration = () => {
+  return (
+    <div class="bg-white shadow p-12 mt-32 max-w-sm mx-auto rounded">
+      <h1 class="mb-6 text-3xl text-center">Registration</h1>
+
+      <form>
+        <label for="email-input" class="block mb-2">
+          Email
+        </label>
+        <input
+          id="email-input"
+          type="email"
+          maxlength="40"
+          required
+          autofocus
+          placeholder="user@example.com"
+          class="block border rounded p-3 mb-3 w-full"
+        />
+        <label for="password-input" class="block mb-2">
+          Password
+        </label>
+        <input
+          id="password-input"
+          type="password"
+          required
+          maxlength="40"
+          class="block border rounded p-3 mb-6 w-full"
+        />
+
+        <button class="block text-lg font-bold bg-red-500 w-full p-3 rounded text-white hover:bg-red-900 hover:shadow-md">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
